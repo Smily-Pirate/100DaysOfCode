8729f7068351e8ae94275317a1c6ce7e6d2925f1
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-10-09 09:27:58 -0700

    Day 15

--- ServerClient_Day14/client.py
+++ ServerClient_Day14/client.py
@@ -1,15 +1,27 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 while True:
     full_msg = ''
+    new_msg = True
     while True:
-       msg = s.recv(8)
-       if len(msg) <= 0:
-           break
+       msg = s.recv(16)
+       if new_msg:
+           print("new msg len:",msg[:HEADERSIZE])
+           msglen: int = (msg[:HEADERSIZE])
+           new_msg = False
+
+       print(f"full message length: {msglen}")
        full_msg += msg.decode("utf-8")
 
-    if len(full_msg) > 0:
-        print(full_msg)
+       print(len(full_msg))
+
+       if len(full_msg) - HEADERSIZE == msglen:
+           print("full msg recvd")
+           print(full_msg[HEADERSIZE:])
+           new_msg = True
+
