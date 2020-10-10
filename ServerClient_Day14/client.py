a3e9d98f8d51d8c951cd89d59878a27f8fd32016
Ghanshyam Joshi <47424158+Ghansu@users.noreply.github.com>
2020-10-10 09:00:02 -0700

    Day 16

--- ServerClient_Day14/client.py
+++ ServerClient_Day14/client.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 HEADERSIZE = 10
 
@@ -6,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 while True:
-    full_msg = ''
+    full_msg = b''
     new_msg = True
     while True:
        msg = s.recv(16)
@@ -16,12 +17,13 @@ while True:
            new_msg = False
 
        print(f"full message length: {msglen}")
-       full_msg += msg.decode("utf-8")
+       full_msg += msg
 
        print(len(full_msg))
 
        if len(full_msg) - HEADERSIZE == msglen:
            print("full msg recvd")
            print(full_msg[HEADERSIZE:])
+           print(pickle.loads((full_msg[HEADERSIZE:])))
            new_msg = True
-
+           full_msg = b''
--- ServerClient_Day14/server.py
+++ ServerClient_Day14/server.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import pickle
 
 HEADERSIZE = 10
 
@@ -12,11 +13,12 @@ while True:
     clientsocket, address = s.accept()
     print(f"Connection from (address) has been established. ")
 
-    msg = "Welcome to the server!"
-    msg = f"{len(msg) :< {HEADERSIZE}}" + msg
-    clientsocket.send(bytes(msg, "utf-8"))
-
-
+    d = {1: "hi", 2: "there"}
+    msg = pickle.dumps(d)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8') + msg
+    print(msg)
+    clientsocket.send(msg)
+    """
     while True:
         time.sleep(3)
         msg = f"The time is {time.time()}"
@@ -24,3 +26,4 @@ while True:
 
         print(msg)
         clientsocket.send(bytes(msg, "utf-8"))
+    """
